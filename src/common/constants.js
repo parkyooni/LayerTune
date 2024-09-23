38372e5e7ece3c4b3c7a841a1a5d29d241e26298
parkyooni <elmi1003@naver.com>
2024-09-23 13:56:27 +0900

    Design: CSS UI setup

---
+++ src/common/constants.js
@@ -0,0 +1,19 @@
+export const targetTags = [
+  "DIV",
+  "SECTION",
+  "ARTICLE",
+  "HEADER",
+  "FOOTER",
+  "ASIDE",
+  "NAV",
+  "MAIN",
+];
+export const commonLayoutClasses = [
+  "container",
+  "wrapper",
+  "wrap",
+  "content",
+  "context",
+  "button",
+  "grid",
+];
